47f5b495c6834e0eef8fca15b948b4b9393c41cd
Srini09git <srinimohand@gmail.com>
2022-12-25 21:46:46 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
+import { Button, TextField } from '@mui/material';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CreateUser />
     </div>
   );
+}
+
+function CreateUser() {
+  return (
+    <div class="login">
+      <h1>Create Account</h1>
+      <div className='text-box'>
+        <TextField id="outlined-basic" label="Name" variant="outlined" />
+        <TextField id="outlined-basic" label="Email" variant="outlined" />
+        <TextField id="outlined-basic" label="Phone Number" variant="outlined" />
+        <Button variant="contained">Submit</Button>
+      </div>
+    </div>
+  )
 }
 
 export default App;
